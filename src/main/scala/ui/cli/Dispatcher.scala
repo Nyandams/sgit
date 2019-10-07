06a801cc93d0a2eb451377c38ec76fe2e63a6c03
Nyandams <lecha.damien@gmail.com>
2019-10-07 10:58:28 +0200

    commit command

--- src/main/scala/ui/cli/Dispatcher.scala
+++ src/main/scala/ui/cli/Dispatcher.scala
@@ -4,7 +4,10 @@ import ui.cli.Parser.getConfig
 object Dispatcher {
   def dispatch(arguments: Array[String]): Unit ={
     getConfig(arguments) match {
-      case Some(config) =>
+      case Some(config) => config.mode match {
+        case "init" => println("init")
+
+      }
       case None =>
     }
   }
