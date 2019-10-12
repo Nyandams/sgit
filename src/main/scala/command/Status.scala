0f4288774d870f22b6e738fdbeb2e6346c8dab04
Nyandams <lecha.damien@gmail.com>
2019-10-13 01:54:01 +0200

    better printing in status

--- src/main/scala/command/Status.scala
+++ src/main/scala/command/Status.scala
@@ -12,13 +12,25 @@ object Status {
    * @param repo
    */
   def status(repo: File): Unit = {
+    val headFile = (repo / ".sgit" / "HEAD").contentAsString.split(" ")(1)
+    val currentBranch = (repo / ".sgit" / headFile).createFileIfNotExists()
+    println(s"On branch ${currentBranch.name}\n")
+
+    if (currentBranch.isEmpty) println("No commits yet\n")
+
     getMapFromIndex(repo) match {
       case Left(mapIndex) =>
         val keys = mapIndex.keySet
 
-        print(getChangesToCommit(repo, keys))
-        print(getNotStagedChanges(repo, mapIndex))
-        print(getUntrackedFiles(repo, keys))
+        val changesToCommit = getChangesToCommit(repo, keys)
+        val notStagedChanges = getNotStagedChanges(repo, mapIndex)
+        val untrackedFiles = getUntrackedFiles(repo, keys)
+
+        if (changesToCommit.nonEmpty) println(changesToCommit)
+        if (notStagedChanges.nonEmpty) println(notStagedChanges)
+        if(untrackedFiles.nonEmpty) println(untrackedFiles)
+
+        if(changesToCommit.isEmpty && notStagedChanges.nonEmpty) println("nothing added to commit but untracked files present (use \"git add\" to track)")
       case Right(error) => println(error)
     }
   }
@@ -57,7 +69,7 @@ object Status {
     val modifiedFilesStringArray = modifiedFiles.map(src => s"\tmodified:   ${src}") mkString "\n"
 
     if(deletedFilesStringArray.nonEmpty || modifiedFilesStringArray.nonEmpty){
-      return s"Changes not staged for commit:\n  (use sgit add/rm <file>... to update what will be committed)\n${RED}${modifiedFilesStringArray}\n${deletedFilesStringArray}${RESET}\n"
+      return s"Changes not staged for commit:\n  (use sgit add/rm <file>... to update what will be committed)\n${RED}${modifiedFilesStringArray}\n${deletedFilesStringArray}${RESET}"
     } else {
       return ""
     }
